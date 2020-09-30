699d606da23a770533aa11241bef1a768597c214
Christian Friedow <christian@friedow.com>
2020-09-30 12:09:43 +0200

    Add file and form routers

---
+++ src/routers/fileRouter.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { IncomingForm } from "formidable";
+
+const router: express.Router = express.Router();
+
+router.post("/", (req, res, next) => {
+  const form = new IncomingForm();
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(fields);
+    console.log(files);
+
+    res.status(200).send("File was successfully submitted!");
+  });
+});
+
+export default router;
