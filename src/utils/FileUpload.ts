e3200e26f6e288961d95f01019248178c4a1f9d0
Daniel <i@dast.xyz>
2021-09-23 16:23:44 +0200

    handle error when multiple files are uploaded

--- src/utils/FileUpload.ts
+++ src/utils/FileUpload.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import path from "path";
 import { IncomingForm, Files, File } from "formidable";
 import { Request } from "express";
 import Formidable from "formidable/Formidable";
@@ -7,7 +8,7 @@ import FhirResourceBuilder from "@/utils/FhirResourceBuilder";
 import { v4 as uuid } from "uuid";
 
 export default class FileUpload {
-  private static getUploadDirectory(req: Request): string {
+  private static uploadDirectory(req: Request): string {
     const context = req.params.context;
     const fhirId = req.params.fhirId;
     return process.env.NODE_ENV === "development"
@@ -21,30 +22,31 @@ export default class FileUpload {
     }
   }
 
+  private static changeFilePath(file: File, uploadDirectory: string): void {
+    if (!FileUpload.filetypeIsWhitelisted(file)) return;
+    const fileExtension = file.path.split(".").pop();
+    const newPath = `${uploadDirectory}/${uuid()}.${fileExtension}`;
+    // this prevents directory traversal attacks
+    // where "../" is used to traverse other directories
+    if (newPath.startsWith(uploadDirectory)) {
+      file.path = newPath;
+    }
+  }
+
   private static initializeUploadForm(uploadDirectory: string): Formidable {
     const form = new IncomingForm({
       keepExtensions: true,
       multiples: false,
       allowEmptyFiles: false,
+      minFileSize: 1,
       maxFieldsSize: 0, // = unlimited
       maxFileSize: 4000 * 1024 * 1024, // = 4 gb
     });
-
-    form.on("fileBegin", function (name, file) {
-      if (!FileUpload.filetypeIsWhitelisted(file)) return;
-      const fileExtension = file.path.split(".").pop();
-      const newPath = `${uploadDirectory}/${uuid()}.${fileExtension}`;
-      // this prevents directory traversal attacks
-      // where "../" is used to traverse other directories
-      if (newPath.startsWith(uploadDirectory)) {
-        file.path = newPath;
-      }
-    });
-
+    form.on("fileBegin", (name, file) => this.changeFilePath(file, uploadDirectory));
     return form;
   }
 
-  private static async parseForm(req: Request, form: Formidable): Promise<Files> {
+  private static async parseRequest(req: Request, form: Formidable): Promise<Files> {
     return new Promise(function (resolve, reject) {
       form.parse(req, function (err, fields, files) {
         if (err !== null) reject(err);
@@ -57,36 +59,60 @@ export default class FileUpload {
     return file.path.match(/\.(mp4|jpg|json|pdf|ply|png)$/i) ? true : false;
   }
 
-  private static urlTo(path: string): string {
+  private static urlOf(path: string): string {
     const protocol =
       process.env.NODE_ENV === "development" || process.env.INSECURE ? "http://" : "https://";
     const url = new URL(path, `${protocol}${process.env.DOMAIN ?? "localhost"}/api/files/`);
     return url.href;
   }
 
-  private static async save(req: Request, uploadDir: string): Promise<File> {
-    const form = FileUpload.initializeUploadForm(uploadDir);
-    const filesMap = await FileUpload.parseForm(req, form);
-    const files = Object.values(filesMap);
-    // TODO delete multiple files if uploaded, mv deletion to here, cover multiple files case
-    if (files.length !== 1) throw new Error("You should submit exactly one file.");
-    // files can be assumed as File because the multiples option of the IncomingForm
-    // was set to false
-    return files[0] as File;
+  private static deleteEmptyDirectories(filepath: string) {
+    const baseDir = path.dirname(filepath).split("/");
+    for (let depth = baseDir.length; depth > 1; depth--) {
+      const dir = baseDir.slice(0, depth).join("/");
+      try {
+        fs.rmdirSync(dir);
+      } catch (e) {
+        // this error happens when files already exists in the dir
+        if (e.code === "ENOTEMPTY") continue;
+        // this error happens when no file is sent. In this
+        // case a temporary file is saved in /tmp/<random>,
+        // but /tmp may not be deleted
+        if (e.code === "EACCES") continue;
+        throw e;
+      }
+    }
   }
 
-  public static async handleUpload(req: Request): Promise<fhir.IAttachment> {
-    const uploadDir = FileUpload.getUploadDirectory(req);
-    FileUpload.createDirIfNotExists(uploadDir);
-    const file = await FileUpload.save(req, uploadDir);
-    const url = FileUpload.urlTo(file.path);
-
-    if (!FileUpload.filetypeIsWhitelisted(file)) {
+  private static deleteFiles(files: File[]) {
+    files.map((file) => {
       fs.unlinkSync(file.path);
+      this.deleteEmptyDirectories(file.path);
+    });
+  }
+
+  private static async save(req: Request, uploadDir: string): Promise<File> {
+    const form = FileUpload.initializeUploadForm(uploadDir);
+    const filesMap = await FileUpload.parseRequest(req, form);
+    const files = Object.values(filesMap) as File[];
+    if (files.length !== 1) {
+      this.deleteFiles(files);
+      throw new Error("You should submit exactly one file.");
+    }
+    if (!FileUpload.filetypeIsWhitelisted(files[0])) {
+      this.deleteFiles(files);
       throw new TypeError(
         "Invalid file type. The server only accepts files with one the following extensions: jpg, json, mp4, pdf, ply or png."
       );
     }
+    return files[0] as File;
+  }
+
+  public static async handleUpload(req: Request): Promise<fhir.IAttachment> {
+    const uploadDir = FileUpload.uploadDirectory(req);
+    FileUpload.createDirIfNotExists(uploadDir);
+    const file = await FileUpload.save(req, uploadDir);
+    const url = FileUpload.urlOf(file.path);
 
     return FhirResourceBuilder.attachment(
       file.type,
