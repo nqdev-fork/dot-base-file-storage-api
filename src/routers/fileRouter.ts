b6e00cede66d967194ddc7e83952a18ee3389938
Daniel <i@dast.xyz>
2021-10-13 22:43:43 +0200

    refactoring

--- src/routers/fileRouter.ts
+++ src/routers/fileRouter.ts
@@ -2,11 +2,11 @@ import express from "express";
 import FileService from "@/services/FileService";
 
 const router: express.Router = express.Router();
-const fileUpload = new FileService();
+const fileService = new FileService();
 
 router.post("/:context/:fhirId", async (req, res) => {
   try {
-    const fhirAttachment = await fileUpload.handleUpload(req);
+    const fhirAttachment = await fileService.handleUpload(req);
     res.status(200).send(fhirAttachment);
   } catch (e) {
     if (e instanceof TypeError) {
